bb71560bbda77309ce699dcbca745d96c5e21a28
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-02-10 12:25:45 +0000

    page interactions

--- tests/acceptance/content.feature
+++ tests/acceptance/content.feature
@@ -3,3 +3,8 @@ Feature: Test that feature have correct content
     Given I am on the blog page
     Then There is a title shown on the page
     And The title tag has content "This is the blog page"
+
+  Scenario: Homepage page has a correct title
+    Given I am on the homepage
+    Then There is a title shown on the page
+    And The title tag has content "This is the homepage"
--- tests/acceptance/steps/content.py
+++ tests/acceptance/steps/content.py
@@ -1,17 +1,17 @@
 from behave import *
 
-from tests.acceptance.page_model.home_page import HomePage
+from tests.acceptance.page_model.base_page import BasePage
 
 use_step_matcher('re')
 
 
 @then('There is a title shown on the page')
 def step_impl(context):
-    page = HomePage(context.browser)
+    page = BasePage(context.driver)
     assert page.title.is_displayed()
 
 
 @step('The title tag has content "(.*)"')
 def step_impl(context, content):
-
-    assert title_tag.text == content
+    page = BasePage(context.driver)
+    assert page.title.text == content
